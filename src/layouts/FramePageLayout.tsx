f0b455696387144994b9b2a6589bde2ad731f1d3
Augusto Assis <augustoflas@outlook.com>
2024-08-21 15:22:31 -0300

    feat: sidebar e navbar as position fixed

--- src/layouts/FramePageLayout.tsx
+++ src/layouts/FramePageLayout.tsx
@@ -4,25 +4,24 @@ import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
 
 const FramePageLayout = () => {
-  return (
-    <div className="d-flex flex-column">
-        <div style={{height: '4.5vh'}}>
-            <NavBar />
+    return (
+        <div className="d-flex flex-column">
+            <div className="position-fixed w-100" style={{ height: '4.5vh', zIndex: 1000 }}>
+                <NavBar />
+                <div style={{ height: '0.5vh' }} className="bg-danger"></div>
+            </div>
+            <div className="container-fluid" style={{ paddingTop: '5vh' }}>
+                <Row>
+                    <Col md={1} className="p-0 position-fixed h-100" style={{ top: '5vh' }}>
+                        <Sidebar />
+                    </Col>
+                    <Col md={{ span: 11, offset: 1 }} className="p-4" style={{ overflowY: 'auto', height: '95vh' }}>
+                        <Outlet />
+                    </Col>
+                </Row>
+            </div>
         </div>
-        <div style={{height: '0.5vh'}} className="bg-danger">
-        </div>
-        <div className="container-fluid" style={{height: '95vh'}}>
-            <Row className="h-100">
-                <Col md={1} className="p-0">
-                    <Sidebar />
-                </Col>
-                <Col md={11} className=" p-4">
-                    <Outlet />
-                </Col>
-            </Row>
-        </div>
-    </div>
-  );
+    );
 };
 
 export default FramePageLayout;
